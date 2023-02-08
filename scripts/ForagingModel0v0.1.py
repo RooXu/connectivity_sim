4ce8548ab9ba6c238b09aa714564173233064093
ajxu-macbookpro <ajxu@haverford.edu>
2023-02-07 20:33:55 -0500

    made some progress

---
+++ scripts/ForagingModel0v0.1.py
@@ -0,0 +1,67 @@
+##########
+# Foraging Model 0  
+# Aaron Xu 
+# 
+# This foraging model samples a gaussian distribution for turning and a travel distance. 
+##########
+
+from typing import Dict, Tuple
+from mpi4py import MPI
+import numpy as np
+from dataclasses import dataclass
+
+from repast4py import core, random, space, schedule, logging, parameters
+from repast4py import context as ctx
+import repast4py
+from repast4py.space import DiscretePoint as dpt
+
+class Walker(core.Agent): 
+    Type = 0 
+    distribution = None
+
+    # The initialization method that runs upon object initialization
+    # The arguments are what I want for my custom walker.
+    def __init__(self, local_object_id: int, rank: int, pt: dpt): 
+        # Initialize the parent class 
+        super().__init__(id=local_object_id,type=Walker.Type,rank=rank)
+        self.pt = pt #Init the variable used for storing agent location.
+    
+    def save(self) -> Tuple:
+        """Saves the state of this Walker as a Tuple.
+        Returns:
+            The saved state of this Walker.
+        """
+        return (self.uid, self.pt.coordinates)
+
+    def walk(self, grid): 
+        # sample from a distribution for theta and r 
+        # convert to x and y coordinates 
+        # move in those directions 
+
+        # <WIP Code> 
+
+        self.pt = grid.move(self, dpt(self.pt.x + xy_dirs[0],\
+                                      self.pt.y + xy_dirs[1],\
+                                      0))
+walker_cache = {} # this cache maintains all the created walkers in this model!
+
+def resore_walker(walker_data: Tuple): 
+    """
+    Args:
+        walker_data: tuple containing the data returned by Walker.save.
+    """
+    # uid is a 3 element tuple: 0 is id, 1 is type, 2 is rank
+    # Transfering data or "unzipping" data from the tuple into individual variables
+    uid = walker_data[0]
+    pt_array = walker_data[2]
+    pt = dpt(pt_array[0], pt_array[1], 0)
+
+    if uid in walker_cache:                     # If the uid exists in the cache
+        walker = walker_cache[uid]              # point to an existing walker 
+    else:
+        walker = Walker(uid[0], uid[2], pt)     # Else create a new
+        walker_cache[uid] = walker
+
+    walker.meet_count = walker_data[1]
+    walker.pt = pt
+    return walker
