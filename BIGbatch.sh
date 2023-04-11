b332f71b53f1f30726b6aa334c561446e66a9913
ajxu-macbookpro <ajxu@haverford.edu>
2023-04-11 00:09:53 -0400

    Bash scripts for batch processing

---
+++ BIGbatch.sh
@@ -0,0 +1,62 @@
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC000_2.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC020_2.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC040_2.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC060_2.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC080_2.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC100_2.yaml
+
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC000_3.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC020_3.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC040_3.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC060_3.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC080_3.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC100_3.yaml
+
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC000_4.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC020_4.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC040_4.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC060_4.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC080_4.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC100_4.yaml
+
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC000_5.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC020_5.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC040_5.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC060_5.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC080_5.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC100_5.yaml
+
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC000_6.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC020_6.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC040_6.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC060_6.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC080_6.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC100_6.yaml
+
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC000_7.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC020_7.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC040_7.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC060_7.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC080_7.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC100_7.yaml
+
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC000_8.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC020_8.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC040_8.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC060_8.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC080_8.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC100_8.yaml
+
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC000_9.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC020_9.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC040_9.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC060_9.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC080_9.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC100_9.yaml
+
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC000_10.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC020_10.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC040_10.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC060_10.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC080_10.yaml
+time python ./scripts/ForagingModel0v0.1.py ./simExp/FC100_10.yaml
---
+++ batchModel.sh
@@ -0,0 +1,4 @@
+time python ./scripts/ForagingModel0v0.1.py ./scripts/FC020_1.yaml
+time python ./scripts/ForagingModel0v0.1.py ./scripts/FC040_1.yaml
+time python ./scripts/ForagingModel0v0.1.py ./scripts/FC060_1.yaml
+time python ./scripts/ForagingModel0v0.1.py ./scripts/FC080_1.yaml
